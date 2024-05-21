a81280aa545cf1523a7659381a540b3dc418dfbe
Farhad Zada <farhad.szd@gmail.com>
2024-05-22 02:09:58 +0400

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,66 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const env = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.2",
+      },
+    ],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    tbsc: {
+      url: env.TBSC_RPC,
+      accounts: [env.PKEY],
+    },
+    bsc: {
+      url: env.BSC_RPC,
+      accounts: [env.PKEY],
+    },
+    sep: {
+      url: env.SEP_RPC,
+      accounts: [env.PKEY],
+    },
+    eth: {
+      url: env.ETH_RPC,
+      accounts: [env.PKEY],
+    },
+    pol: {
+      url: env.POL_RPC,
+      accounts: [env.PKEY],
+    },
+    tpol: {
+      url: env.TPOL_RPC,
+      accounts: [env.PKEY],
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+  etherscan: {
+    apiKey: {
+      bsc: env.BSC_APIKEY
+    }
+  }
+};
